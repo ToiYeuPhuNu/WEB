40f2c1b2987e69d6814d85154c90dbf9a51aa25e
hoang121160 <danghoang121101@gmail.com>
2023-09-19 17:48:55 +0700

    update service code by hoangkhong

--- src/main/java/project/WebGioiThieuSanPham/controller/CategoryController.java
+++ src/main/java/project/WebGioiThieuSanPham/controller/CategoryController.java
@@ -1,4 +1,33 @@
 package project.WebGioiThieuSanPham.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import project.WebGioiThieuSanPham.dto.categoryDto.request.CategoryRequest;
+import project.WebGioiThieuSanPham.dto.categoryDto.response.CategoryResponse;
+import project.WebGioiThieuSanPham.service.category.CategoryService;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/category")
+@AllArgsConstructor
 public class CategoryController {
+    CategoryService categoryService;
+    @PostMapping("/add")
+    public CategoryResponse createCategory(@PathVariable(name = "categoryRequest") CategoryRequest categoryRequest){
+        return categoryService.createCategory(categoryRequest);
+    }
+    @PutMapping("/{id}")
+    public CategoryResponse updateCategory(UUID categoryId, CategoryRequest updateCategoryRequest){
+        return categoryService.updateCategory(categoryId, updateCategoryRequest);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteCategory(UUID categoryId){
+        categoryService.deleteCategory(categoryId);
+    }
+    @GetMapping("/")
+    public List<CategoryResponse> getAllCategory(){
+        return categoryService.getAllCategories();
+    }
 }
--- src/main/java/project/WebGioiThieuSanPham/controller/ClothesController.java
+++ src/main/java/project/WebGioiThieuSanPham/controller/ClothesController.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 
 import org.springframework.web.bind.annotation.*;
 
+import project.WebGioiThieuSanPham.dto.clothesDto.request.ClothesRequest;
+import project.WebGioiThieuSanPham.dto.clothesDto.response.ClothesDetailView;
+import project.WebGioiThieuSanPham.dto.clothesDto.response.ClothesResponse;
 import project.WebGioiThieuSanPham.models.Clothes;
 import project.WebGioiThieuSanPham.service.clothes.ClothesServiceImpl;
 
@@ -21,37 +24,22 @@ public class ClothesController {
     ClothesServiceImpl clothesService;
 
     @PostMapping("/")
-    public Clothes add(@RequestBody Clothes clothes) {
-        return clothesService.add(clothes);
+    public ClothesResponse createClothes(@RequestBody ClothesRequest clothesRequest) {
+        return clothesService.createClothes(clothesRequest);
     }
 
     @GetMapping("/{id}")
-    public Clothes getDetail(@PathVariable(name = "id") UUID id)
-    {
-        return clothesService.getById(id);
+    public ClothesDetailView getClothesById(@PathVariable(name = "id") UUID id){
+        return clothesService.getClothesById(id);
     }
 
-//    @GetMapping("/filter")
-//    public ResponseEntity<ApiBaseObjectResponse<Page<ClothesAvatarView>>> filterClothes(
-//            @RequestParam(required = false) String category,
-//            @RequestParam(required = false) String color,
-//            @RequestParam(required = false) Sex sex,
-//            @RequestParam(required = false) BigDecimal minPrice,
-//            @RequestParam(required = false) BigDecimal maxPrice,
-//            @PageableDefault(sort = "releaseDate") Pageable pageable) {
-//        try {
-//            Page<ClothesAvatarView> filteredClothes = clothesService.filterClothes(
-//                    category,
-//                    color,
-//                    sex,
-//                    minPrice,
-//                    maxPrice,
-//                    pageable
-//            );
-//            return new ResponseEntity<>(new ApiBaseObjectResponse<>("MDS-000", "Success", filteredClothes), HttpStatus.OK);
-//        } catch (Exception e) {
-//            return new ResponseEntity<>(new ApiBaseObjectResponse<>("MDS-004", "Lỗi trong quá trình tìm kiếm sản phẩm", null), HttpStatus.BAD_REQUEST);
-//        }
-//    }
+    @PutMapping("/{id}")
+    public ClothesResponse updateClothes(@PathVariable(name = "id") UUID id, @RequestBody ClothesRequest clothesRequest){
+        return clothesService.updateClothes(id,clothesRequest);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteColthes(@PathVariable(name = "id") UUID id){
+        clothesService.deleteClothes(id);
+    }
 }
 
